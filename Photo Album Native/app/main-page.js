fc75452324633e905043ac58c20698b2e68f4b81
Dag Ole Kristoffersen <thedole@gmail.com>
2015-03-14 21:27:40 +0100

    Added images, viewmodel and databinding

--- Photo Album Native/app/main-page.js
+++ Photo Album Native/app/main-page.js
@@ -1,1 +1,10 @@
 // Application logic
+var modelModule = require("./view-model");
+var model = new modelModule.PhotoAlbumModel();
+
+exports.onPageLoaded = onPageLoaded;
+
+function onPageLoaded(args) {
+	var page = args.object;
+	page.bindingContext = model;
+}
---
+++ Photo Album Native/app/view-model.js
@@ -0,0 +1,48 @@
+var observable = require("data/observable");
+var observableArrayModule = require("data/observable-array");
+var imageSourceModule = require("image-source");
+var fileSystemModule = require("file-system");
+
+var array = new observableArrayModule.ObservableArray();
+var directory = "/res/";
+
+var item1 = { itemImage: imageFromSource("01.jpg") };
+var item2 = { itemImage: imageFromSource("02.jpg") };
+var item3 = { itemImage: imageFromSource("03.jpg") };
+var item4 = { itemImage: imageFromSource("04.jpg") };
+var item5 = { itemImage: imageFromSource("05.jpg") };
+var item6 = { itemImage: imageFromSource("06.jpg") };
+var item7 = { itemImage: imageFromSource("07.jpg") };
+var item8 = { itemImage: imageFromSource("08.jpg") };
+array.push([item1, item2, item3, item4, item5, item6]);
+var item7 = { itemImage: imageFromSource("09.jpg") };
+var item8 = { itemImage: imageFromSource("10.jpg") };
+
+function imageFromSource(imageName) {
+	return imageSourceModule.fromFile(fileSystemModule.path.join(__dirname, directory + imageName));
+};
+
+var __extends = this.__extends || function (d, b) {
+	for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+	function __() { this.constructor = d; }
+	__.prototype = b.prototype;
+	d.prototype = new __();
+};
+
+var PhotoAlbumModel = (function (_super) {
+	__extends(PhotoAlbumModel, _super);
+	function PhotoAlbumModel() {
+		_super.call(this);
+	}
+	return PhotoAlbumModel;
+})(observable.Observable);
+
+Object.defineProperty(PhotoAlbumModel.prototype, "photoItems", {
+	get: function () {
+		return array;
+	},
+	enumerable: true,
+	configurable: true
+});
+
+exports.PhotoAlbumModel = PhotoAlbumModel;
