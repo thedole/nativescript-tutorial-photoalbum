e8fda03eec951e6f1ce5b76ab1d0bb561d6ee001
Dag Ole Kristoffersen <thedole@gmail.com>
2015-03-14 21:45:08 +0100

    added buttonTap to exprots, added some styling to button

--- Photo Album Native/app/main-page.js
+++ Photo Album Native/app/main-page.js
@@ -3,6 +3,7 @@ var modelModule = require("./view-model");
 var model = new modelModule.PhotoAlbumModel();
 
 exports.onPageLoaded = onPageLoaded;
+exports.buttonTap = buttonTap;
 
 function onPageLoaded(args) {
 	var page = args.object;
