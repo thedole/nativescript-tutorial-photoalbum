f2ba26bd135ad81986168abf0d3fff4b73ba8013
Dag Ole Kristoffersen <thedole@gmail.com>
2015-03-14 23:02:19 +0100

    Attempt at reading images from server

--- Photo Album Native/app/view-model.js
+++ Photo Album Native/app/view-model.js
@@ -8,19 +8,6 @@ var fileSystemModule = require("file-system");
 
 
 var array = new observableArrayModule.ObservableArray();
-var directory = "/res/";
-
-var item1 = { itemImage: imageFromSource("01.jpg") };
-var item2 = { itemImage: imageFromSource("02.jpg") };
-var item3 = { itemImage: imageFromSource("03.jpg") };
-var item4 = { itemImage: imageFromSource("04.jpg") };
-var item5 = { itemImage: imageFromSource("05.jpg") };
-var item6 = { itemImage: imageFromSource("06.jpg") };
-array.push([item1, item2, item3, item4, item5, item6]);
-var item7 = { itemImage: imageFromSource("07.jpg") };
-var item8 = { itemImage: imageFromSource("08.jpg") };
-var item9 = { itemImage: imageFromSource("09.jpg") };
-var item10 = { itemImage: imageFromSource("10.jpg") };
 
 function imageFromSource(imageName) {
 	return imageSourceModule.fromFile(fileSystemModule.path.join(__dirname, directory + imageName));
@@ -51,32 +38,19 @@ Object.defineProperty(PhotoAlbumModel.prototype, "photoItems", {
 });
 
 PhotoAlbumModel.prototype.tapAction = function () {
-	array.push(item7);
-	array.push(item8);
-	array.push(item9);
-	array.push(item10);
+	everlive.Files.get()
+		.then(function (files) {
+			var len = files.length;
 
-	this.set("message", "Images added. Total images: " + array.length);
-
-	var image,
-		file,
-		len = array.length;
-
-	for (i = 0; i < len; i++) {
-		image = array.pop();
-		file = {
-			"Filename": Math.random().toString(36).substring(2, 15) + ".jpg",
-			"ContentType": "image/jpeg",
-			"base64": image.itemImage.toBase64String("JPEG", 100)
-		};
-
-		everlive.Files.create(file,
-			function (data) {
-			},
-			function (error) {
-				array.push(image);
-			});
-	}
+			//file = {
+			//	"Filename": Math.random().toString(36).substring(2, 15) + ".jpg",
+			//	"ContentType": "image/jpeg",
+			//	"base64": image.itemImage.toBase64String("JPEG", 100)
+			//};
+			for (var i = 0; i < len; i++) {
+				array.push(files[i].base64);
+			}
+		});
 };
 
 exports.PhotoAlbumModel = PhotoAlbumModel;
